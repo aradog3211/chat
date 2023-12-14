240bdea960479527e658eb5f16d94a90f1f8fb31
aradog3211 <aradogferreira@gmail.com>
2023-12-14 19:00:07 +0000

    Make it better

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require("express")
 const fs = require("fs")
 const os = require("os")
+const cors = require("cors")
 
-app = express()
+var app = express()
+app.use(cors())
 
 app.get("/", (req, res) => {
 	console.log("OK")
-	fs.readFile('data.txt', "utf8", function(err, data) {
-	  console.log(data)
+	res.send("OK")
+})
+
+app.put("/login", (req, res) => {
+	const username = req.headers.username
+	const password = req.headers.password
+	
+	fs.readFile('auth.txt', "utf8", function(err, data) {
+		data = data.split("\n")
+		data.pop()
+		let auth = {}
+		for (var x of data) {
+			auth[x.split(" ")[0]] = x.split(" ")[1]
+		}
+		if (auth[username]) {
+			if (auth[username] == password) {
+				res.send("success")
+			} else {
+				res.send("wrong password")
+			}
+		} else {
+			res.send("invalid username")
+		}
 	});
 	
-	res.send("OK")
 })
 
 const PORT = process.env.PORT || 3000
